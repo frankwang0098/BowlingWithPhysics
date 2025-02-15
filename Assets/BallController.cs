bc187430835bb58280b375a378635f71e5c4ec86
Frank Wang <frankwang0098@gmail.com>
2025-02-15 14:51:49 -0800

    pins converted to prefabs

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -7,6 +7,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private InputManager inputManager;
 
     private Rigidbody ballRB;
+    private bool isBallLaunched;
 
     void Start()
     {
@@ -21,6 +22,11 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
+        if (isBallLaunched)
+        {
+            return;
+        }
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
