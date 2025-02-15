4873ad38137b59239f9b3b4379fa1693b536b199
Frank Wang <frankwang0098@gmail.com>
2025-02-15 14:35:40 -0800

    Forgot to set up github right away. But have finished setting up ball and bowling pins

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
+
     void Start()
     {
-        
+        //Grabbing a reference to RigidBody
+        ballRB = GetComponent<Rigidbody>();
+
+        // Add a listener to the OnSpacePressed event.
+        // When the space key is pressed the
+        // LaunchBall method will be called.
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LaunchBall()
     {
-        
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
+
 }
